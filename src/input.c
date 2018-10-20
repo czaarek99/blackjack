0a81e104c5b0e18231a2aac698561d1097d16606
czaarek99 <sarek123456789@gmail.com>
2018-10-20 11:27:56 +0200

    Rename input_size in require_input to text_input_size

--- src/input.c
+++ src/input.c
@@ -12,14 +12,14 @@ void get_input_discard_overflow(char *input, short input_size) {
     }
 }
 
-void require_input(void* input, short input_size,
+void require_input(void* input, short text_input_size,
         verify_input_func verify_func,
         on_bad_input_func bad_input_func,
         on_good_input_func good_input_func) {
 
     while(true) {
-        char* text_input = calloc(input_size, 1);
-        get_input_discard_overflow(text_input, input_size);
+        char* text_input = calloc(text_input_size, 1);
+        get_input_discard_overflow(text_input, text_input_size);
         bool verified = (*verify_func)(text_input, input);
         if(verified) {
             (*good_input_func)(input);
--- src/input.h
+++ src/input.h
@@ -8,7 +8,7 @@ typedef void (*on_good_input_func)(void*);
 
 
 void get_input_discard_overflow(char *input, short input_size);
-void require_input(void* input, short input_size,
+void require_input(void* input, short text_input_size,
         verify_input_func verify_func,
         on_bad_input_func bad_input_func,
         on_good_input_func good_input_func);
